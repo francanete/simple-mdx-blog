06c5fdf7befbce6de9c211e457409b9783dcba01
Fran Canete <francanetedev@gmail.com>
2021-08-19 22:23:20 +0100

    added new files

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { ThemeProvider } from "styled-components"
+import Theme from "./src/themes/theme"
+
+
+export const wrapRootElement =  ({ element }) => (
+  <ThemeProvider theme={Theme}>
+    {element}
+  </ThemeProvider>
+)
